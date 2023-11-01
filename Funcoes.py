84b8e6f884398cd19be1cfbeff7cdb42d2d92270
Gabriel Dallemole <th3g3ntl3m3n@duck.com>
2023-10-31 22:47:40 -0300

    mais funções

--- Funcoes.py
+++ Funcoes.py
@@ -1,6 +1,14 @@
 def escopo():
-    x = 1
-    print(x)
+    x = 10
     
+    def outra_funcao():
+        x = 11
+        y = 2
+        print(x, y)
+        
+    outra_funcao()
+    print(x)
 
+
+print(x)
 escopo()
