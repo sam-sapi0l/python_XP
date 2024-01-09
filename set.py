455a9d97149db6c4f91de17448181937f3199e01
Gabriel Dallemole <th3g3ntl3m3n@duck.com>
2024-01-09 09:51:39 -0300

    exercicios utilizando set, funções e listas

--- set.py
+++ set.py
@@ -14,3 +14,18 @@ s1.update('Pereira')
 #s1.clear()
 s1.discard('Pereira')
 print(s1)
+
+
+# Operações úteis
+# união | união (union) - Une
+# intersecção % (intersection) - Itens presentes em ambos
+# diferença - Itens presentes apenas no set da esquerda
+# diferença simétrica ^ - Itens que não estão em amboas
+
+s1 = {1, 2, 3}
+s2 = {2, 3, 4}
+s3 = s1 | s2
+s3 = s1 & s2
+s3 = s2 - s1
+s3 = s1 ^ s2
+print(s3)
