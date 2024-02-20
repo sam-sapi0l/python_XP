ea815e0a0ebc72c88853c525d256a050e4f01b19
Gabriel Dallemole <th3g3ntl3m3n@duck.com>
2024-02-20 17:47:29 -0300

    aula 109 até aula 111

---
+++ aula110.py
@@ -0,0 +1,27 @@
+# GroupBy - agrupando valores (itertools)
+
+from itertools import groupby
+
+
+alunos = [
+    {'nome': 'Gabriel', 'nota': 'A'},
+    {'nome': 'Leticia', 'nota': 'B'},
+    {'nome': 'Fabrício', 'nota': 'A'},
+    {'nome': 'Rosemary', 'nota': 'C'},
+    {'nome': 'Joana', 'nota': 'D'},
+    {'nome': 'João', 'nota': 'A'},
+    {'nome': 'Eduardo', 'nota': 'B'},
+    {'nome': 'André', 'nota': 'A'},
+    {'nome': 'Anderson', 'nota': 'C'},
+]
+
+def ordena(aluno):
+    return aluno['nota']
+
+
+alunos_agrupados = sorted(alunos, key=ordena)
+grupos = groupby(alunos_agrupados, key=ordena)
+
+for chave, grupo in grupos:
+    print(chave)
+    print(list(grupo))
