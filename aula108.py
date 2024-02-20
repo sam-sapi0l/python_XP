36c9e37484ce0ebd4d1ba2e21019f213e1e5434e
Gabriel Dallemole <th3g3ntl3m3n@duck.com>
2024-02-20 15:04:15 -0300

    exercicios com itertools e também aula108 e 109

---
+++ aula108.py
@@ -0,0 +1,21 @@
+# Count é um iterador sem fim definido (itertools)
+from itertools import count
+c1 = count()
+r1 = range(10)
+
+print('c1', hasattr(c1, '__iter__'))
+print('c1', hasattr(c1, '__next__'))
+print('r1', hasattr(r1, '__iter__'))
+print('r1', hasattr(r1, '__next__'))
+
+print('count')
+for i in c1:
+        if i >= 100:
+            break
+
+        print(i)
+
+print()
+print('range')
+for i in r1:
+      print(i)
---
+++ aula109.py
@@ -0,0 +1,7 @@
+# Combinations, Permutations e Product - Itertools
+# Combinação - Ordem não importa - iterável + tamanho do grupo
+# Permutação - Ordem importa
+# Produto - Ordem importa e repete valores únicos
+
+
+
